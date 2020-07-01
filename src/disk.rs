3b4d1d9fb78a7208d75b098e6f560b760b9b489c
Parker Timmerman <parker@parkertimmerman.com>
2020-07-01 01:26:34 -0400

    adding DADiskCopyDescription

--- src/disk.rs
+++ src/disk.rs
@@ -1,5 +1,6 @@
 #![deny(missing_debug_implementations)]
 
+use core_foundation as cf;
 use libc::{
     c_char,
     c_void,
@@ -13,4 +14,6 @@ pub type DADiskRef = *const _DADisk;
 #[link(name = "DiskArbitration", kind = "framework")]
 extern "C" {
     pub fn DADiskGetBSDName(disk: DADiskRef) -> *const c_char;
+
+    pub fn DADiskCopyDescription(disk: DADiskRef) -> cf::dictionary::CFDictionaryRef;
 }
